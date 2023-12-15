746b8d54605c108e0a1571f54a9e5bd45ea36086
dianck <73984084+dianck@users.noreply.github.com>
2023-12-15 17:06:47 +0700

    u

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -27,11 +27,12 @@ class mahasiswa_model{
 
     public function tambahDataPenghuni($data){
         $query = "INSERT INTO `penghuni` VALUES ('', :nama, :nama_panggilan, :tempat_kerja)";
+        /*
         $this->db->query($query);
         $this->bind('nama', $data['nama']);
         $this->bind('nama_panggilan', $data['nama_panggilan']);
         $this->bind('tempat_kerja', $data['tempat_kerja']);
-
+        */
         //$this->db->execute();
         
         return 1;
