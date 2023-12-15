acf3c8be11782d586b131471f18df09f70a5eab6
dianck <73984084+dianck@users.noreply.github.com>
2023-12-15 15:01:31 +0700

    edit tombol modal

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,8 +2,8 @@
 <div class="container mt-3">
     <div class="row">
         <div class="col-10">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Launch demo modal
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                    Tambah Data
             </button>
             <br><br>
             <h3>Daftar Mahasiswa</3>
@@ -23,11 +23,11 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="judulModal">Tambah Data</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -35,7 +35,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">Tambah Data</button>
       </div>
     </div>
   </div>
