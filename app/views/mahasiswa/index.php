db8abcb9095dc8184830cce3e118698d6736efde
dianck <73984084+dianck@users.noreply.github.com>
2023-12-15 14:55:47 +0700

    update tambah button modal di halaman app/view/index

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,11 @@
 
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-10">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+            </button>
+            <br><br>
             <h3>Daftar Mahasiswa</3>
             <ul class="list-group">
                 <?php foreach( $data['mhs'] as $mhs){ ?>
@@ -15,4 +19,24 @@
         </div>
     </div>
 
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
