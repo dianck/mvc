863d7be61aa032f9ff1b21cab5e2b87eff7e398c
dianck <73984084+dianck@users.noreply.github.com>
2023-12-16 08:46:01 +0700

    execute

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -34,9 +34,9 @@ class mahasiswa_model{
         //$this->bind('nama_panggilan', $data['nama_panggilan']);
         //$this->bind('tempat_kerja', $data['tempat_kerja']);
 
-        //$this->db->execute();
+        $this->db->execute();
         
-        return 1;
-        //return $this->db->rowCount();
+        //return 1;
+        return $this->db->rowCount();
     }
 }
