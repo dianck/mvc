4e65bef38e1211791b482f50ab3ba1d0d061b4a8
dianck <73984084+dianck@users.noreply.github.com>
2023-12-21 15:55:02 +0700

    c1

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -5,13 +5,30 @@
         <?php flasher::flash(); ?>
       </div>
     </div>
+    
+    <div class="row mb-3">
+      <div class="col-lg-6">
+            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
+                    Tambah Data
+            </button>      
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div class="col-lg-6">
+        <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+              <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            </div>
+        </form>
+      </div>
+    </div>
+
 
     <div class="row">
         <div class="col-10">
-            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
-                    Tambah Data
-            </button>
-            <br><br>
+
             <h3>Daftar Mahasiswa</3>
             <ul class="list-group">
                 <?php foreach( $data['mhs'] as $mhs){ ?>
