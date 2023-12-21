7db91d4372c3f989dcca345e2e3c09f706c3716f
dianck <73984084+dianck@users.noreply.github.com>
2023-12-21 16:17:37 +0700

    c7

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -75,9 +75,10 @@ class mahasiswa_model{
         $query = "SELECT * FROM PENGHUNI WHERE nama like '%".$keyword."%' ";
         //$query = "SELECT FROM PENGHUNI WHERE nama like :keyword ";
 
-        $this->db->query($query);
+        var_dump($query);
+        //$this->db->query($query);
         //$this->db->bind('keyword', "%$keyword%");
 
-        return $this->db->resultSet();
+        //return $this->db->resultSet();
     }
 }
